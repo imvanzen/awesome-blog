0e289b8cfb60356a5cd72412b3077cf55edc1f5f
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 00:27:27 +0100

    Simplify rule for css

--- webpack.config.js
+++ webpack.config.js
@@ -33,16 +33,8 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {},
-          },
-        ],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
