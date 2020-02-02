c06431341d176e47921f0c3a1473e9592c5f5511
Jakub Reczko <contact@jakubreczko.com>
2020-02-02 13:52:36 +0100

    Update webpack dev config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PORT = 3000;
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: path.join(__dirname, './src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-enc'],
+        },
+      },
+      {
+        test: /\.(html)$/,
+        use: 'html-loader',
+        options: {
+          attrs: [':data-src'],
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: 'dist',
+    liveReload: false,
+    port: PORT,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'dist/index.html',
+    }),
+  ],
 };
