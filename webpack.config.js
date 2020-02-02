4003ad4ffa474e4d5e84f03df3d3ab5215271efe
Jakub Reczko <contact@jakubreczko.com>
2020-02-02 23:52:54 +0100

    Add loader for css files which uses css modules

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+      },
     ],
   },
   devServer: {
