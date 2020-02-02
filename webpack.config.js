251e00e8cae6bc54f70eb72460431af6ef965193
Jakub Reczko <contact@jakubreczko.com>
2020-02-02 13:38:53 +0100

    Specify entry point and bundle file name

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './path/to/my/entry/file.js',
+    entry: path.join(__dirname, './src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'my-first-webpack.bundle.js'
-    }
+        filename: 'bundle.js'
+    },
 };
