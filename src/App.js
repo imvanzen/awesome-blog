1e1e2dd2708739e53b027a6c63564dd1d79a9678
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 00:15:34 +0100

    Include styles to the root component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, NotFound } from './components/pages';
 
+import './App.css';
+
 function App() {
   return (
     <div className="App">
