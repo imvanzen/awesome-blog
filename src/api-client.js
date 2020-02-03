5f1f67e5a1d4eb846b0c0d5637c459f9e3b5ae55
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 19:29:56 +0100

    use axios to get single post

--- src/api-client.js
+++ src/api-client.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export default {
   getPosts: () => axios.get(`${BASE_URL}/posts`),
-  getPost: (id) => {},
+  getPost: id => axios.get(`${BASE_URL}/posts/${id}`),
   getComments: (postId) => {},
   createComment: (postId) => {},
 };
