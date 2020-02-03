a181d80cfa9f8a29f96f9b4cbbc3ac1879a7fe8a
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 19:38:06 +0100

    use axios to get comments for single post

--- src/api-client.js
+++ src/api-client.js
@@ -5,6 +5,6 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 export default {
   getPosts: () => axios.get(`${BASE_URL}/posts`),
   getPost: id => axios.get(`${BASE_URL}/posts/${id}`),
-  getComments: (postId) => {},
+  getComments: postId => axios.get(`${BASE_URL}/posts/${postId}/comments`),
   createComment: (postId) => {},
 };
