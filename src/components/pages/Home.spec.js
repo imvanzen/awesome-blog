48d006429ad8b5989ab96b8fd2618eea2163385b
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 00:56:20 +0100

    Add tests for home template

---
+++ src/components/pages/Home.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Navigation from '../templates/Navigation';
+import Footer from '../templates/Footer';
+import Home from './Home';
+
+describe('Home Page', () => {
+  const wrapper = mount(<Home />);
+
+  test('should render Navigation', () => {
+    expect(wrapper.find(Navigation)).toHaveLength(1);
+  });
+
+
+
+  test('should render Footer', () => {
+    expect(wrapper.find(Footer)).toHaveLength(1);
+  });
+});
