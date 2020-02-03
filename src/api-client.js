117e22b4298d0e7354fbd7d000c33b54ce067324
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 19:24:26 +0100

    get rid of async/await

--- src/api-client.js
+++ src/api-client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export default {
-  getPosts: async () => axios.get(`${BASE_URL}/posts`),
+  getPosts: () => axios.get(`${BASE_URL}/posts`),
   getPost: (id) => {},
   getComments: (postId) => {},
   createComment: (postId) => {},
