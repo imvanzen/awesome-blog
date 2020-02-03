1f04ae6cbb13e4b54d599d6a8730c1ec5edfe849
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 17:17:47 +0100

    Add sample object response of posts list

--- __tests__/api-client.spec.js
+++ __tests__/api-client.spec.js
@@ -1,6 +1,18 @@
 import apiClient from '../src/api-client';
 
 describe('Api Client', () => {
+  const testPostsList = [
+    {
+      id: '123',
+      title: 'Lorem ipsum',
+      content: 'Lorem ipsum',
+    }, {
+      id: '456',
+      title: 'Ipsum lorem',
+      content: 'Ipsum lorem',
+    },
+  ];
+
   it('Get posts', () => {
 
   });
