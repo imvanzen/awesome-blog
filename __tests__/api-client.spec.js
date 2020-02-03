7b0077c34ac232f021d16e0008cbc21585a808b4
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 17:15:28 +0100

    Create basic tests for api requests

---
+++ __tests__/api-client.spec.js
@@ -0,0 +1,19 @@
+import apiClient from '../src/api-client';
+
+describe('Api Client', () => {
+  it('Get posts', () => {
+
+  });
+
+  it('Get post by ID', () => {
+
+  });
+
+  it('Get comments for post by post id', () => {
+
+  });
+
+  it('Create comment for post by post id', () => {
+
+  });
+});
