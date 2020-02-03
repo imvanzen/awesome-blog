44f7d6eed9e924511dfc76fb9d82d015ca605cd5
Jakub Reczko <contact@jakubreczko.com>
2020-02-03 18:48:15 +0100

    Use axios to get posts

--- src/api-client.js
+++ src/api-client.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
 export default {
-  getPosts: () => {},
+  getPosts: async () => axios.get(`${BASE_URL}/posts`),
   getPost: (id) => {},
   getComments: (postId) => {},
   createComment: (postId) => {},
