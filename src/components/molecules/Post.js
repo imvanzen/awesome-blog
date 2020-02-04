9a82ed27bdd75d46db77ebac3d00a229575194d1
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 18:09:21 +0100

    Fix problem with Post links

--- src/components/molecules/Post.js
+++ src/components/molecules/Post.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Post.css';
 
 const Post = ({ post: { title, body, id } }) => (
   <article className={styles.Post}>
     <h2>{ title }</h2>
     <p>{ body }</p>
-    <BrowserRouter><Link to={`/posts/${id}`}>Czytaj więcej</Link></BrowserRouter>
+    <Link to={`/posts/${id}`}>Czytaj więcej</Link>
   </article>
 );
 
