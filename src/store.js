26ff4c68e7b7c3482d3672739f078bbfd85538ed
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 17:42:10 +0100

    Change way how urls work

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from './reducers';
 
-export const history = createBrowserHistory();
+export const history = createHashHistory();
 
 export default function configureStore(preloadedState) {
   const store = createStore(
