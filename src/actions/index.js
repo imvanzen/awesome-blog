106a782b7e4b765a42616c37e54d52728ae29cc0
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 13:49:35 +0100

    Define actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import { getPosts } from '../api-client';
+import { FETCH_ALL_POSTS } from './types';
 
+export const fetchPosts = (posts) => {
+  return {
+    type: FETCH_ALL_POSTS,
+    posts,
+  };
+};
+
+export const fetchAllPosts = () => dispatch => getPosts()
+  .then(posts => dispatch(fetchPosts(posts)))
+  .catch((error) => {
+    throw (error);
+  });
