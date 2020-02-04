8a74fd5564569935d27e1822411c148f27fcc017
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 15:20:14 +0100

    update params

--- src/components/molecules/Post.js
+++ src/components/molecules/Post.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import { BrowserRouter, Link } from 'react-router-dom';
 import styles from './Post.css';
 
-const Post = ({ post }) => (
+const Post = ({ post: { title, body, id } }) => (
   <article className={styles.Post}>
-    <h2>{ post.title }</h2>
-    <p>{ post.body }</p>
-    <BrowserRouter><Link to={`/posts/${post.id}`}>Czytaj więcej</Link></BrowserRouter>
+    <h2>{ title }</h2>
+    <p>{ body }</p>
+    <BrowserRouter><Link to={`/posts/${id}`}>Czytaj więcej</Link></BrowserRouter>
   </article>
 );
 
