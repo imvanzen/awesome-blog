a7d29f1aa25e48d86a1462fa4e6bf6b9456a918a
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 13:32:38 +0100

    Remove key from props

--- src/components/molecules/Post.js
+++ src/components/molecules/Post.js
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 import { BrowserRouter, Link } from 'react-router-dom';
 import styles from './Post.css';
 
-const Post = ({ title, body, key }) => (
+const Post = ({ title, body, id }) => (
   <article className={styles.Post}>
     <h2>{ title }</h2>
     <p>{ body }</p>
-    { key ? <BrowserRouter><Link to={`/posts/${key}`}>Czytaj więcej</Link></BrowserRouter> : null }
+    <BrowserRouter><Link to={`/posts/${id}`}>Czytaj więcej</Link></BrowserRouter>
   </article>
 );
 
 Post.propTypes = {
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Post;
