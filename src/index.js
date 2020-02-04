3ad1bd0d22b27e0ba06500a84bc8294ec84184e6
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 13:49:58 +0100

    Fetch posts on load

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
 import configureStore, { history } from './store';
 import App from './App';
+import { fetchAllPosts } from './actions/index';
 
 const store = configureStore({});
+
+store.dispatch(fetchAllPosts());
 
 ReactDOM.render(
   <Provider store={store}>
