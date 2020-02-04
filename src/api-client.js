5386150d34c80f716b0fbe242484386bb3a4e289
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 18:24:46 +0100

    Add API connector

--- src/api-client.js
+++ src/api-client.js
@@ -4,8 +4,10 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 const getPosts = () => axios.get(`${BASE_URL}/posts`).then(response => response.data);
 const getPost = id => axios.get(`${BASE_URL}/posts/${id}`).then(response => response.data);
+const getPostComments = postId => axios.get(`${BASE_URL}/posts/${postId}/comments`).then(response => response.data);
 
 export {
   getPosts,
   getPost,
+  getPostComments,
 };
