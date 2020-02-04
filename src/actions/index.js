92e45a81c5ba5a165fb40f826d796900867d315c
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 17:23:32 +0100

    Add post details actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-import { getPosts } from '../api-client';
-import { FETCH_ALL_POSTS } from './types';
+import { getPost, getPosts } from '../api-client';
+import { FETCH_ALL_POSTS, FETCH_POST } from './types';
 
 export const fetchAllPostsSuccess = (posts) => {
   return {
@@ -8,8 +8,21 @@ export const fetchAllPostsSuccess = (posts) => {
   };
 };
 
+export const fetchPostSuccess = (post) => {
+  return {
+    type: FETCH_POST,
+    post,
+  };
+};
+
 export const fetchAllPosts = () => dispatch => getPosts()
   .then(posts => dispatch(fetchAllPostsSuccess(posts)))
+  .catch((error) => {
+    throw (error);
+  });
+
+export const fetchPost = id => dispatch => getPost(id)
+  .then(post => dispatch(fetchPostSuccess(post)))
   .catch((error) => {
     throw (error);
   });
