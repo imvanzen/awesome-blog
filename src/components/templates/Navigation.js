8a69ebb547e59ff8c9b6b3b2da8dc70e46deef3b
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 18:10:11 +0100

    Fix navigation link

--- src/components/templates/Navigation.js
+++ src/components/templates/Navigation.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Navigation.css';
 
 const Navigation = () => (
   <nav className={styles.Navigation}>
-    <BrowserRouter>
-      <Link to='/'>
-        <span className={styles.NavigationLogo}>Awesome Blog</span>
-      </Link>
-    </BrowserRouter>
+    <Link to="/">
+      <span className={styles.NavigationLogo}>Awesome Blog</span>
+    </Link>
   </nav>
 );
 
