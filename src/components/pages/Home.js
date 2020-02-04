9bfae725cc506541b76214e2035728d8f8764800
Jakub Reczko <contact@jakubreczko.com>
2020-02-04 01:11:59 +0100

    Add PostsList to the Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navigation from '../templates/Navigation';
 import Footer from '../templates/Footer';
+import PostsList from '../templates/PostsList';
 
 const Home = () => (
   <div className='Home'>
     <Navigation />
-
+    <PostsList posts={[]} />
     <Footer />
   </div>
 );
